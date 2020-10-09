87b755ef967dba379e2cdc0e29b079da535c5734
Vishnu Khanth <vishnukhanthb@gmail.com>
2020-10-09 12:40:01 +0530

    Update main.py

--- Task0/Task0.2/Video Processing/Codes/main.py
+++ Task0/Task0.2/Video Processing/Codes/main.py
@@ -3,10 +3,22 @@ import numpy as np
 import os
 
 def partA():
-    pass
+    os.chdir('../Videos')
+    video = cv2.VideoCapture('RoseBloom.mp4')
+    # getting the frame rate for given video.
+    fps = video.get(5)
+    # setting the frame to the start of 6th second.
+    video.set(1, fps*5)
+    ret, frame = video.read()
+    os.chdir('../Generated')
+    cv2.imwrite('frame_as_6.jpg', frame)
 
 def partB():
-    pass
+    img = cv2.imread('frame_as_6.jpg', 1)
+    # setting blue and green channel to 0.
+    img[:, :, 0] = 0
+    img[:, :, 1] = 0
+    cv2.imwrite('frame_as_6_red.jpg', img)
 
 partA()
 partB()
